6db871919efa8236da961962450b24364a9e1157
Aden <adenwells@live.co.uk>
2018-08-16 17:02:57 +0100

    added different page templates

---
+++ page-our-services.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * The template for displaying pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<?php
+		// Start the loop.
+		while ( have_posts() ) : the_post();
+
+			// Include the page content template.
+			get_template_part( 'template-parts/content', 'page' );
+
+			// End of the loop.
+		endwhile;
+
+
+		$query_args = array(
+			'post_type' => 'services',
+			'posts_per_page' => -1,
+			'orderby' => 'rand'
+		);
+
+		$result = new WP_Query( $query_args );
+
+		if ( $result->have_posts() ) {
+			while ( $result->have_posts() ) : $result->the_post();
+			
+				get_template_part( 'template-parts/content', 'page-our-services' );
+			
+			endwhile;
+			wp_reset_query();
+		}
+		
+
+		?>
+
+	</main><!-- .site-main -->
+
+	<?php get_sidebar( 'content-bottom' ); ?>
+
+</div><!-- .content-area -->
+
+<?php get_footer(); ?>
---
+++ template-parts/content-page-our-services.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * The template used for displaying page content
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+
+	<?php twentysixteen_post_thumbnail(); ?>
+
+	<div class="entry-content">
+		<div class="service-content-top">
+
+		<img src="<?php the_field('icon'); ?>" />
+
+		<p><?php the_field('description'); ?></p>
+		</div>
+		<?php
+		
+		if( have_rows('pricing') ): ?>
+
+			<table>
+				<tr>
+					<th>Job</th>
+					<th>1 Hour</th>
+					<th>2 Hours</th>
+					<th>3-5 Hours</th>
+					<th>5+ Hours</th>
+				</tr>
+			</table>
+			
+			<?php // loop through the rows of data
+		    while ( have_rows('pricing') ) : the_row(); 
+	   
+			   // display a sub field value ?>
+				<table>
+					<tr>
+						<td><?php the_sub_field('job'); ?></td>
+						<td><?php the_sub_field('1_hour'); ?></td>
+						<td><?php the_sub_field('2_hours'); ?></td>
+						<td><?php the_sub_field('3-5_hours'); ?></td>
+						<td><?php the_sub_field('5+_hours'); ?></td>
+					</tr>
+				</table>				
+	   
+		    <?php endwhile;
+	   
+	   else :
+	   
+		   // no rows found
+	   
+	   endif;
+
+
+		wp_link_pages( array(
+			'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentysixteen' ) . '</span>',
+			'after'       => '</div>',
+			'link_before' => '<span>',
+			'link_after'  => '</span>',
+			'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>%',
+			'separator'   => '<span class="screen-reader-text">, </span>',
+		) );
+		?>
+	</div><!-- .entry-content -->
+
+	<?php
+		edit_post_link(
+			sprintf(
+				/* translators: %s: Name of current post */
+				__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
+				get_the_title()
+			),
+			'<footer class="entry-footer"><span class="edit-link">',
+			'</span></footer><!-- .entry-footer -->'
+		);
+	?>
+
+</article><!-- #post-## -->
