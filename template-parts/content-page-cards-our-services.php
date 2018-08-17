a681194bfa4fc6c64c597057424c6a694a3928f2
Aden <adenwells@live.co.uk>
2018-08-17 11:07:20 +0100

    Added the content pages for service cards to go on home and services page

---
+++ template-parts/content-page-cards-our-services.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The template used for displaying page content
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+?>
+
+<a href="#post-<?php the_title(); ?>">
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+
+	<?php twentysixteen_post_thumbnail(); ?>
+
+	<?php
+		edit_post_link(
+			sprintf(
+				/* translators: %s: Name of current post */
+				__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
+				get_the_title()
+			),
+			'<footer class="entry-footer"><span class="edit-link">',
+			'</span></footer><!-- .entry-footer -->'
+		);
+	?>
+
+</article><!-- #post-## -->
+</a>
